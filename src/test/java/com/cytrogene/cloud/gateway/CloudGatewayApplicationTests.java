607fa012beb675a432443b2912e79939f4d935da
Aditya <adityajagtap149@gmail.com>
2022-01-29 17:44:34 +0530

    first commit

---
+++ src/test/java/com/cytrogene/cloud/gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cytrogene.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
